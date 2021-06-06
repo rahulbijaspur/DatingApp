a74b26ebe516518e617f2d6465518e30dad527b2
rahulbijaspur <66760433+rahulbijaspur@users.noreply.github.com>
2021-06-06 11:41:00 +0530

    end of section5

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,45 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BuggyController :ControllerBase
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return StatusCode(401,"Unauthorised");
+        }
+
+         [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+            if (thing==null) return NotFound();
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetSeverError()
+        {
+            var thing = _context.Users.Find(-1);
+            var thingToReturn = thing.ToString();
+            return thingToReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
